24a2b7011294ee49fe627f59eb3331b52499d750
Abdul Manor <asyunus688@gmail.com>
2025-02-03 08:52:03 +0000

    default location at uni campus

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
+// mapboxgl.accessToken = 'pk.eyJ1IjoiYXMtbWFub3IiLCJhIjoiY202bzllaG56MTA3djJrczRjeW5pdWl1eSJ9.BgZnQvFmOZrVJhsSCeHPiA';
+// navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true})
+
+// function successLocation(position) {
+//     setupMap([position.coords.longitude, position.coords.latitude])
+// }
+
+// function errorLocation() {
+//     setupMap([-1.7789778666324583, 53.64292387582586])
+// }
+
+// function setupMap(center) {
+//     var map = new mapboxgl.Map({
+//         container: 'map',
+//         style: 'mapbox://styles/mapbox/streets-v11',
+//         center: center,
+//         zoom: 16
+//       })
+      
+//     const nav = new mapboxgl.NavigationControl();
+//     map.addControl(nav);
+
+//     var directions = new MapboxDirections({
+//         accessToken: mapboxgl.accessToken
+//     });
+//     map.addControl(directions, 'top-left')
+// }
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXMtbWFub3IiLCJhIjoiY202bzllaG56MTA3djJrczRjeW5pdWl1eSJ9.BgZnQvFmOZrVJhsSCeHPiA';
-navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true})
 
-function successLocation(position) {
-    setupMap([position.coords.longitude, position.coords.latitude])
-}
-
-function errorLocation() {
-    setupMap([-1.7789778666324583, 53.64292387582586])
-}
-
-function setupMap(center) {
+function setupMap() {
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
-        center: center,
+        center: [-1.7789778666324583, 53.64292387582586], // University of Huddersfield
         zoom: 16
-      })
-      
+    });
+
     const nav = new mapboxgl.NavigationControl();
     map.addControl(nav);
 
     var directions = new MapboxDirections({
         accessToken: mapboxgl.accessToken
     });
-    map.addControl(directions, 'top-left')
+    map.addControl(directions, 'top-left');
 }
+
+// Initialize map at University of Huddersfield
+setupMap();
